44a0f8e491645fdfac1d69087880408cad29c6b9
evt13 <isaacmerenciodesouza@gmail.com>
2025-04-29 21:47:16 -0300

    Create ex-1.py

---
+++ ex-1.py
@@ -0,0 +1,11 @@
+#) Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis.
+
+a=10
+b=20
+temporaria=b
+print(a,b)
+
+a=b
+b=temporaria
+print(a,temporaria)
+# terminar
