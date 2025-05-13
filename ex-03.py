954294e82f8ceade6901b0ae669a41bb5881be8f
evt13 <isaacmerenciodesouza@gmail.com>
2025-05-13 20:39:21 -0300

    Create ex-03.py

---
+++ ex-03.py
@@ -0,0 +1,16 @@
+#3) Os pares de instruções abaixo produzem o mesmo resultado? Imprima os valores abaixo, veja se algum deles (A, B ou C) possuem valores diferentes nas versões 1 e 2, e caso sim, explique num comentário o motivo.
+
+
+A1 = (4/2)+(2/4)
+print ("A1=",A1)
+B1 = 4/(2+2)/4
+print ("B1=",B1)
+C1 = (4+2)*2-4
+print ("C1=",C1)
+A2 = 4/2+2/4
+print ("A2=",A2)
+B2 = 4/2+2/4
+print ("B2=",B2)
+C2 = 4+2*2-4
+print ("C2=",C2)
+# OS PARENTESES VÃO DIZER A ORDEM E A PRIORIDADE DA EQUAÇÃO#
