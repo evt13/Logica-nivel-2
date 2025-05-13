f552976241c1b7bcb7baaf78a480064823c6836e
evt13 <isaacmerenciodesouza@gmail.com>
2025-05-13 20:40:17 -0300

    Update ex-03.py

--- ex-03.py
+++ ex-03.py
@@ -1,5 +1,9 @@
 #3) Os pares de instruções abaixo produzem o mesmo resultado? Imprima os valores abaixo, veja se algum deles (A, B ou C) possuem valores diferentes nas versões 1 e 2, e caso sim, explique num comentário o motivo.
+Intuito do exercício: O desafio proposto por este exercício é que o aluno entenda se os valores do lado esquerdo e direito dão os mesmo resultados ou não e, quando diferentes, qual a razão de chegar nesse resultado.
 
+A1 ← (4/2)+(2/4)	e	A2 ← 4/2+2/4
+B1 ← 4/(2+2)/4	e	B2 ← 4/2+2/4
+C1 ← (4+2)*2-4	e	C2 ← 4+2*2-4
 
 A1 = (4/2)+(2/4)
 print ("A1=",A1)
