1289e0f58ea8d0841a617b1e66c100d5401bb731
evt13 <isaacmerenciodesouza@gmail.com>
2025-05-14 16:25:11 -0300

    Create ex-05.py

---
+++ ex-05.py
@@ -0,0 +1,9 @@
+#5) Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+
+
+base=int(input())
+print("base do triangulo=",base)
+altura=int(input())
+print("altura do triangulo=",altura)
+area_triangulo=base*altura/2
+print("area do triangulo é",area_triangulo)
